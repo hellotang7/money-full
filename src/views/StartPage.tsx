aecf8894fe5d5a9d6a8dc01f406427bd6b63fe56
hellotang7 <2714148252@qq.com>
2023-07-30 23:15:09 +0800

    创建MainLayout布局

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,9 +4,9 @@ import s from "./StartPage.module.scss";
 import { FloartButton } from "../shared/FloartButton";
 import { Center } from "../shared/Center";
 import { Icon } from "../shared/Icon";
-import { Navbar } from "../shared/Navbar";
 import { Overlay } from "../shared/Overlay";
 import { RouterLink } from "vue-router";
+import { MainLayout } from "../layouts/MainLayout";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -15,31 +15,33 @@ export const StartPage = defineComponent({
       overlayVisble.value = !overlayVisble.value;
     };
     return () => (
-      <div>
-        <Navbar>
-          {{
-            default: () => "山竹记账",
-            icon: () => (
-              <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
-            ),
-          }}
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
+      <MainLayout>
+        {{
+          title: () => "山竹记账",
+          icon: () => (
+            <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
+          ),
+          default: () => (
+            <>
+              <Center class={s.pig_wrapper}>
+                <Icon name="pig" class={s.pig} />
+              </Center>
 
-        <div class={s.button_wrapper}>
-          <RouterLink to="/items/create">
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloartButton iconName="add"></FloartButton>
-        </RouterLink>
-        {overlayVisble.value && (
-          <Overlay onClose={() => (overlayVisble.value = false)} />
-        )}
-      </div>
+              <div class={s.button_wrapper}>
+                <RouterLink to="/items/create">
+                  <Button class={s.button}>开始记账</Button>
+                </RouterLink>
+              </div>
+              <RouterLink to="/items/create">
+                <FloartButton iconName="add"></FloartButton>
+              </RouterLink>
+              {overlayVisble.value && (
+                <Overlay onClose={() => (overlayVisble.value = false)} />
+              )}
+            </>
+          ),
+        }}
+      </MainLayout>
     );
   },
 });
