bfdc5abd38d6487350cf4dcef9b515b6a31500d4
hellotang7 <2714148252@qq.com>
2023-07-30 16:16:09 +0800

    修复了menu-icon不显示问题，添加了Navbar组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -4,6 +4,7 @@ import s from "./StartPage.module.scss";
 import { FloartButton } from "../shared/FloartButton";
 import { Center } from "../shared/Center";
 import { Icon } from "../shared/Icon";
+import { Navbar } from "../shared/Navbar";
 
 export const StartPage = defineComponent({
   setup: (props, context) => {
@@ -12,14 +13,19 @@ export const StartPage = defineComponent({
     };
     return () => (
       <div>
-        <nav>menu</nav>
+        <Navbar>
+          {{
+            default: "山竹记账",
+            icon: <Icon name="menu" class={s.navIcon} />,
+          }}
+        </Navbar>
         <Center class={s.pig_wrapper}>
-          <Icon class={s.pig} name="pig" />
+          <Icon name="pig" />
         </Center>
 
         <div class={s.button_wrapper}>
           <Button class={s.button} onClick={onClick}>
-            测试
+            开始记账
           </Button>
         </div>
         <FloartButton iconName="add"></FloartButton>
