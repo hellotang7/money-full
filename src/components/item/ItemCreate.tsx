06c7bffc95d3dae9a29c04042c51606034fc51b9
hellotang7 <2714148252@qq.com>
2023-08-03 00:45:59 +0800

    完成记一笔的样式

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -12,19 +12,111 @@ export const ItemCreate = defineComponent({
   },
   setup: (props, context) => {
     const refKind = ref("支出");
+    const refExpensesTags = ref([
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+      { id: 1, name: "餐费", sign: "￥", caregory: "expenses" },
+      { id: 2, name: "打车", sign: "￥", caregory: "expenses" },
+      { id: 3, name: "租房", sign: "￥", caregory: "expenses" },
+    ]);
+    const refIncomeTags = ref([
+      { id: 4, name: "工资", sign: "￥", caregory: "income" },
+      { id: 5, name: "发传单", sign: "￥", caregory: "income" },
+      { id: 6, name: "诈骗", sign: "￥", caregory: "income" },
+      { id: 4, name: "工资", sign: "￥", caregory: "income" },
+      { id: 5, name: "发传单", sign: "￥", caregory: "income" },
+      { id: 6, name: "诈骗", sign: "￥", caregory: "income" },
+      { id: 4, name: "工资", sign: "￥", caregory: "income" },
+      { id: 5, name: "发传单", sign: "￥", caregory: "income" },
+      { id: 6, name: "诈骗", sign: "￥", caregory: "income" },
+      { id: 4, name: "工资", sign: "￥", caregory: "income" },
+      { id: 5, name: "发传单", sign: "￥", caregory: "income" },
+      { id: 6, name: "诈骗", sign: "￥", caregory: "income" },
+      { id: 4, name: "工资", sign: "￥", caregory: "income" },
+      { id: 5, name: "发传单", sign: "￥", caregory: "income" },
+      { id: 6, name: "诈骗", sign: "￥", caregory: "income" },
+    ]);
     return () => (
-      <MainLayout>
+      <MainLayout class={s.layout}>
         {{
           title: () => "记一笔",
           icon: () => <Icon name="left" class={s.navIcon} />,
           default: () => (
             <>
-              <Tabs v-model:selected={refKind.value}>
-                <Tab name="支出">icon 列表1</Tab>
-                <Tab name="收入">icon 列表2</Tab>
-              </Tabs>
-              <div class={s.inputPad_wrapper}>
-                <InputPad />
+              <div class={s.wrapper}>
+                <Tabs v-model:selected={refKind.value} class={s.tabs}>
+                  <Tab name="支出" class={s.tags_wrapper}>
+                    <div class={s.tag}>
+                      <div class={s.sign}>
+                        <Icon name="add" class={s.createTag} />
+                      </div>
+                      <div class={s.name}>新增</div>
+                    </div>
+                    {refExpensesTags.value.map((tag) => (
+                      <div class={[s.tag, s.selected]}>
+                        <div class={s.sign}>{tag.sign}</div>
+                        <div class={s.name}>{tag.name}</div>
+                      </div>
+                    ))}
+                  </Tab>
+                  <Tab name="收入" class={s.tags_wrapper}>
+                    <div class={s.tag}>
+                      <div class={s.sign}>
+                        <Icon name="add" class={s.createTag} />
+                      </div>
+                      <div class={s.name}>新增</div>
+                    </div>
+                    {refIncomeTags.value.map((tag) => (
+                      <div class={[s.tag, s.selected]}>
+                        <div class={s.sign}>{tag.sign}</div>
+                        <div class={s.name}>{tag.name}</div>
+                      </div>
+                    ))}
+                  </Tab>
+                </Tabs>
+                <div class={s.inputPad_wrapper}>
+                  <InputPad />
+                </div>
               </div>
             </>
           ),
