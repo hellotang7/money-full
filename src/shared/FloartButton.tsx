05a4d9f6e06557a78dc3c786f3cfe270358f6b79
hellotang7 <2714148252@qq.com>
2023-08-10 23:47:24 +0800

    美化折线图

--- src/shared/FloartButton.tsx
+++ src/shared/FloartButton.tsx
@@ -1,6 +1,11 @@
 import { PropType, defineComponent } from "vue";
 import { Icon, IconName } from "./Icon";
 import s from "./FloartButton.module.scss";
+import {RouterLink} from 'vue-router';
+
+
+
+
 export const FloartButton = defineComponent({
   props: {
     iconName: {
@@ -10,9 +15,11 @@ export const FloartButton = defineComponent({
   },
   setup: (props, context) => {
     return () => (
+        <RouterLink to={'/items/create'}>
       <div class={s.floatButton}>
         <Icon name={props.iconName} class={s.icon} />
       </div>
+        </RouterLink>
     );
   },
 });
