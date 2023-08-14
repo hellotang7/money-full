5a437166b5ee6ecf1d64ac0361681da52ecdb9ce
hellotang7 <2714148252@qq.com>
2023-08-14 20:11:33 +0800

    解决了一个&&的bug

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -6,7 +6,7 @@ import { InputPad } from "./InputPad";
 import { useRouter } from "vue-router";
 import { http } from "../../shared/Http";
 import { Tags } from "./Tags";
-import { Dialog } from "vant";
+import {Dialog, Toast} from 'vant';
 import { AxiosError } from "axios";
 import { BackIcon } from "../../shared/BackIcon";
 import {hasError, validate} from '../../shared/validate';
@@ -27,39 +27,37 @@ export const ItemCreate = defineComponent({
     });
     const errors = reactive<FormErrors<typeof  formData>>({kind: [], tag_ids: [], amount: [], happen_at: []})
     const router = useRouter();
-    const onError = (error: AxiosError<ResourceError>) => {
-      if (error.response?.status === 422) {
-        Dialog.alert({
-          title: "出错",
-          message: Object.values(error.response.data.errors).join("\n标签类型"),
-        });
-      }
-      throw error;
-    };
+    // const onError = (error: AxiosError<ResourceError>) => {
+    //   if (error.response?.status === 422) {
+    //     Dialog.alert({
+    //       title: "出错",
+    //       message: Object.values(error.response.data.errors).join("\n标签类型"),
+    //     });
+    //   }
+    //   throw error;
+    // };
     const onSubmit = async () => {
-      // Object.assign(errors, { kind: [], tag_ids: [], amount: [], happen_at: [] })
-      // Object.assign(errors, validate(formData, [
-      //   { key: 'kind', type: 'required', message: '类型必填' },
-      //   { key: 'tag_ids', type: 'required', message: '标签必填' },
-      //   { key: 'amount', type: 'required', message: '金额必填' },
-      //   { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为零' },
-      //   { key: 'happen_at', type: 'required', message: '时间必填' },
-      // ]));
-      // if (hasError(errors)) {
-      //   Dialog.alert({
-      //     title: "提示",
-      //     message: Object.values(errors).filter(i=>i.length>0).join("\n"),
-      //   })
-      //   return
-      // }
+      Object.assign(errors, { kind: [], tag_ids: [], amount: [], happen_at: [] })
+      Object.assign(errors, validate(formData, [
+        { key: 'kind', type: 'required', message: '类型必填' },
+        { key: 'tag_ids', type: 'required', message: '标签必填' },
+        { key: 'amount', type: 'required', message: '金额必填' },
+        { key: 'amount', type: 'notEqual', value: 0, message: '金额不能为零' },
+        { key: 'happen_at', type: 'required', message: '时间必填' },
+      ]));
+      if (hasError(errors)) {
+        Dialog.alert({
+          title: "提示",
+          message: Object.values(errors).filter(i=>i.length>0).join("\n"),
+        })
+        return
+      }
 
         await http
-            .post<Resource<Item>>('/items', formData, {_mock: 'itemCreate', _autoLoading: true})
-            .catch(onError);
-        // Dialog.alert({
-        //   title: "成功",
-        //   message: "添加成功",
-        // });
+            .post<Resource<Item>>('/items', formData, {_mock: 'itemCreate'})
+            // .catch(onError);
+
+
         router.push('/items');
 
     };
